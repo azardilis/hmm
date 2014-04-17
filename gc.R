1bf53c15a6ea5ffe6c1b1bcdbb4825b9cf14580e
azardilis <azardilis@gmail.com>
2014-04-18 00:12:32 +0100

    changed some variables names

--- gc.R
+++ gc.R
@@ -1,17 +1,19 @@
-DiscretiseCG <- function(f, k) {
-    # k  number of bins to divide into
+## ---- Discretise ----
+DiscretiseCG <- function(f, nbins) {
+    # f      file containing CG-content percentages
+    # nbins  number of bins to divide into
 
     cg <- as.matrix(read.table(f))
     cg <- as.vector(cg)
 
-    ival <- (max(cg) - min(cg)) / k
+    ival <- (max(cg) - min(cg)) / nbins
 
     bins <- ceiling(cg / i)
-    bins[bins == 0] = 1 #because ceiling(0)=0
+    bins[bins == 0] <- 1 #because ceiling(0)=0
 
     return(bins)
 }
 
-bins <- DiscretiseCG("data/cg.dat", 5)
+bins <- DiscretiseCG("data/cg.dat", nbins=5)
 write.table(bins, "data/dsCG.dat", row.names=FALSE, col.names=FALSE)
 
